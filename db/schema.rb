d68433a6991db48536a3c7571d40aeb74a0dab1c
SteveTannahill <steve@tattoohero.com>
2016-03-29 01:50:57 -0400

    changed db to postgres for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160323041840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "labels", force: :cascade do |t|
     t.string   "name"
     t.string   "colour"
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 20160323041840) do
     t.integer "label_id"
   end
 
-  add_index "labels_users", ["label_id"], name: "index_labels_users_on_label_id"
-  add_index "labels_users", ["user_id"], name: "index_labels_users_on_user_id"
+  add_index "labels_users", ["label_id"], name: "index_labels_users_on_label_id", using: :btree
+  add_index "labels_users", ["user_id"], name: "index_labels_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160323041840) do
     t.integer  "role"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
